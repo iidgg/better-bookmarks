601aa8d55ec79b657fec6b7efff8d142d30daad3
idrni <idrni@icloud.com>
2023-07-31 02:22:38 +0300

    disabling some code "temp"

--- scripts/index.js
+++ scripts/index.js
@@ -7,20 +7,52 @@ async function random_function() {
     console.log(a); //? << This gives u tree, yay have an apple
   });
 }
-async function create_bookmark_folder() {
-  folder_number++;
-  chrome.bookmarks.create({ parentId: "1" , title: `New folder (${folder_number})` }, function (newFolder) {
-    console.log("added folder: " + newFolder.title);
-  });
-}
-async function create_bookmark(bookmark_title, bookmark_url) {
-    chrome.bookmarks.create({ title: bookmark_title, url: bookmark_url }, function (newBookmark) {
-        console.log("added bookmark: " + newBookmark.title);
-});
-}
 
-folder_button.addEventListener("click",() => {
-    // create_bookmark_folder(); => this is for creating a folder we don't need it for now
-    create_bookmark(bookmark_title.value, bookmark_url.value);  // pass the tittle and url of the bookmark (bookmark_title.value, bookmark_url.value)
-} ); 
 random_function();
+// Too early to code such a thing down there
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// async function create_bookmark_folder() {
+//   folder_number++;
+//   chrome.bookmarks.create(
+//     { parentId: "1", title: `New folder (${folder_number})` },
+//     function (newFolder) {
+//       console.log("added folder: " + newFolder.title);
+//     }
+//   );
+// }
+// async function create_bookmark(bookmark_title, bookmark_url) {
+//   chrome.bookmarks.create(
+//     { title: bookmark_title, url: bookmark_url },
+//     function (newBookmark) {
+//       console.log("added bookmark: " + newBookmark.title);
+//     }
+//   );
+// }
+
+// folder_button.addEventListener("click", () => {
+//   // create_bookmark_folder(); => this is for creating a folder we don't need it for now
+//   create_bookmark(bookmark_title.value, bookmark_url.value); // pass the tittle and url of the bookmark (bookmark_title.value, bookmark_url.value)
+// });
