da63e54f000cdb08f98fd13b04a2a433845c859e
idrni <idrni@icloud.com>
2023-07-30 16:20:42 +0300

    Who i am i? modified:   popups/default.html Meh, nothing just use ur eyes. Added title which looks shit but feels cool ngl
    modified:   scripts/index.js
    aaaaaa tested smth
    
    modified:   styles/style.css
    font and background that look dusty

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-console.log("dad watch me code");
+async function random_function() {
+  chrome.bookmarks.getTree().then((a, b, c) => {
+    console.log(a); //? << This gives u tree, yay have an apple
+  });
+}
+random_function();
