6522b4f68438583c2b6e4104253db613cc57ddb0
zombie22 <fatihkabbani223@gmail.com>
2023-07-30 20:12:36 +0200

    usless create folder function (test)     added new button to index.html [= 	modified:   popups/default.html
        create_bookmark_folder added and look to the code and you will understand
    	modified:   scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
+let folder_button = document.getElementById("folder_button");
+let folder_number = 0;
 async function random_function() {
   chrome.bookmarks.getTree().then((a, b, c) => {
     console.log(a); //? << This gives u tree, yay have an apple
   });
 }
+async function create_bookmark_folder() {
+  folder_number++;
+  chrome.bookmarks.create({ parentId: "1" , title: `New folder (${folder_number})` }, function (newFolder) {
+    console.log("added folder: " + newFolder.title);
+  });
+}
+folder_button.addEventListener("click", create_bookmark_folder); 
 random_function();
