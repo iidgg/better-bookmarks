3c25046941637346f7fdcfb92ad31dfae8f62603
idrni <idrni@icloud.com>
2023-07-31 03:01:38 +0300

    The tree

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,25 @@
-const folder_button = document.getElementById("folder_button");
-const bookmark_title = document.getElementById("bookmark_title");
-const bookmark_url = document.getElementById("bookmark_Url");
-let folder_number = 0;
-async function random_function() {
-  chrome.bookmarks.getTree().then((a, b, c) => {
-    console.log(a); //? << This gives u tree, yay have an apple
+chrome.bookmarks.getTree().then((tree) => {
+  console.log(tree); //? << This gives u tree, yay have an apple
+  goThroughBranch(tree);
+});
+
+function goThroughBranch(array) {
+  array.forEach((e) => {
+    if (e.id === "0") return goThroughBranch(e.children);
+    const listElement = document.createElement(e.children ? "ul" : "li");
+    console.log(`treeID-${e.parentId || "0"}`);
+    const bookmarkList = document.getElementById(`treeID-${e.parentId || "0"}`);
+    listElement.id = `treeID-${e.id}`;
+    listElement.textContent = e.title || `"${e.url}"`;
+
+    bookmarkList.appendChild(listElement);
+    if (e.children && e.children.length > 0) goThroughBranch(e.children);
+    // if its a folder it may have some children's so we go through
+    // it may also don't have any which will also cause the code to ignore it
+    // Because do we need code that we don't use?
   });
 }
 
-random_function();
 // Too early to code such a thing down there
 //
 //
@@ -23,6 +34,10 @@ random_function();
 //
 //
 //
+// const folder_button = document.getElementById("folder_button");
+// const bookmark_title = document.getElementById("bookmark_title");
+// const bookmark_url = document.getElementById("bookmark_Url");
+// let folder_number = 0;
 //
 //
 //
