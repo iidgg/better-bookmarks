43040a0510d35de83333338ac2c2c26aa29a6410
idrni <idrni@icloud.com>
2023-08-01 04:22:07 +0300

    i don't know how to describe this

--- scripts/data/tags.js
+++ scripts/data/tags.js
@@ -1,22 +1,24 @@
 import { set, get, remove, getAll, removeAll } from "./storage.js";
 
+const tagsStorageName = "tags";
+
 export async function createTag(name) {
   if (!tagOperationHandler()) return false;
-  const tags = await get("tags");
+  const tags = await get(tagsStorageName);
   const originalTagsLength = tags.length;
   const sameTag = findTag(name);
   if (sameTag) return false;
   // ^^ Verify that no tag have the same name, tags have unique names
   tags.push(name);
   // ^^ add the tag to the tags array
-  if (tags.length - 1 === originalTagsLength) set("tags", tags);
+  if (tags.length - 1 === originalTagsLength) set(tagsStorageName, tags);
   //   ^^ Verify that it have added only and only one tag
   return true;
 }
 
 export async function removeTag(name) {
   if (!tagOperationHandler()) return false;
-  const tags = await get("tags");
+  const tags = await get(tagsStorageName);
   const originalTagsLength = tags.length;
   const sameTag = findTag(name);
   if (!sameTag) return false;
@@ -24,22 +26,22 @@ export async function removeTag(name) {
   const indexOfTag = tags.indexOf(name);
   tags.splice(indexOfTag, indexOfTag);
   // ^^ removed the tag from the tags array
-  if (tags.length + 1 === originalTagsLength) set("tags", tags);
+  if (tags.length + 1 === originalTagsLength) set(tagsStorageName, tags);
   //   ^^ Verify that it have removed only and only one tag
   return true;
 }
 
 export async function findTag(name) {
   if (!tagOperationHandler()) return false;
-  const tags = await get("tags");
+  const tags = await get(tagsStorageName);
   return await tags.find((tag) => tag === name)[0];
 }
 
 async function debugAllTagOperations() {
-  console.log(await get("tags"));
+  console.log(await get(tagsStorageName));
   console.log(await createTag("coolTag"));
 
-  console.log(await get("tags"));
+  console.log(await get(tagsStorageName));
   console.log(await removeTag("coolTag"));
 }
 
@@ -49,9 +51,9 @@ async function tagOperationHandler() {
   // Verify that the tags value is available in the storage
   //? Creates one if non are found
   // returns false in rare cases where the creation fails
-  const tags = await get("tags");
+  const tags = await get(tagsStorageName);
   if (!tags) {
-    const tagsCreate = await set("tags", []);
+    const tagsCreate = await set(tagsStorageName, []);
     if (tagsCreate) return true;
     return false;
   }
