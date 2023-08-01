594d4f719c0270bba7a09651232c327399a42427
idrni <idrni@icloud.com>
2023-08-01 04:17:02 +0300

    removeTag operation and more modified:   scripts/data/tags.js

--- scripts/data/tags.js
+++ scripts/data/tags.js
@@ -4,7 +4,7 @@ export async function createTag(name) {
   if (!tagOperationHandler()) return false;
   const tags = await get("tags");
   const originalTagsLength = tags.length;
-  const sameTag = await tags.find((tag) => tag === name);
+  const sameTag = findTag(name);
   if (sameTag) return false;
   // ^^ Verify that no tag have the same name, tags have unique names
   tags.push(name);
@@ -14,14 +14,41 @@ export async function createTag(name) {
   return true;
 }
 
+export async function removeTag(name) {
+  if (!tagOperationHandler()) return false;
+  const tags = await get("tags");
+  const originalTagsLength = tags.length;
+  const sameTag = findTag(name);
+  if (!sameTag) return false;
+  // ^^ Verify the tag existence
+  const indexOfTag = tags.indexOf(name);
+  tags.splice(indexOfTag, indexOfTag);
+  // ^^ removed the tag from the tags array
+  if (tags.length + 1 === originalTagsLength) set("tags", tags);
+  //   ^^ Verify that it have removed only and only one tag
+  return true;
+}
+
+export async function findTag(name) {
+  if (!tagOperationHandler()) return false;
+  const tags = await get("tags");
+  return await tags.find((tag) => tag === name);
+}
+
 async function debugAllTagOperations() {
   console.log(await get("tags"));
   console.log(await createTag("coolTag"));
+
+  console.log(await get("tags"));
+  console.log(await removeTag("coolTag"));
 }
 
 debugAllTagOperations();
 
 async function tagOperationHandler() {
+  // Verify that the tags value is available in the storage
+  //? Creates one if non are found
+  // returns false in rare cases where the creation fails
   const tags = await get("tags");
   if (!tags) {
     const tagsCreate = await set("tags", []);
