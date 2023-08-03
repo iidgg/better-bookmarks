122fb075455a7c21ff9b64f6092154d6a3629234
idrni <idrni@icloud.com>
2023-08-03 08:31:26 +0300

    it creates in main

--- scripts/bookmark-create.js
+++ scripts/bookmark-create.js
@@ -1,11 +1,11 @@
 const bookmark_button = document.getElementById("bookmark-button");
 // const bookmark_title = document.getElementById("bookmark-title");
-// create bookmarks on click with the current tab url and title if no title is given 
+// create bookmarks on click with the current tab url and title if no title is given
 bookmark_button.addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const tab = tabs[0];
-    chrome.bookmarks.create({ title:  tab.title , url: tab.url })
+    chrome.bookmarks.create({ parentId: "1", title: tab.title, url: tab.url });
     // chrome.bookmarks.create({ title: bookmark_title.value || tab.title , url: tab.url })
-      // bookmark_title.value = "";
+    // bookmark_title.value = "";
   });
 });
