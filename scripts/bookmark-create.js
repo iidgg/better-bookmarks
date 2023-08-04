aa244529e308afe9614fab53f92ac4f476c394df
zombie22 <fatihkabbani223@gmail.com>
2023-08-04 17:42:11 +0200

    readded the bookmark create function (was deleted by mistake)
    	modified:   scripts/bookmark-create.js

--- scripts/bookmark-create.js
+++ scripts/bookmark-create.js
@@ -4,6 +4,7 @@ const bookmark_button = document.getElementById("bookmark-button");
 bookmark_button.addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const tab = tabs[0];
+    chrome.bookmarks.create({ title: tab.title, url: tab.url });
 
     const newBookmark = { title: tab.title, url: tab.url };
     // call managebookmarkdata function to save the data to the storage
