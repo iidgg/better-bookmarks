4d376160b6e58a9c5aa2ffb4b2a8534a580d4b89
zombie22 <fatihkabbani223@gmail.com>
2023-08-04 09:54:00 +0200

    Refactor bookmark data management into separate function and to a separate file
        removed anything that was not needed in bookmark-create.js file and moved it to bookmark-data-management.js file
    	modified:   scripts/bookmark-create.js
    	new file:   scripts/bookmark-data-management.js

--- scripts/bookmark-create.js
+++ scripts/bookmark-create.js
@@ -1,6 +1,4 @@
-import { get, set, remove, removeAll } from "./data/storage.js";
-
-const bookmarkObject = { bookmarks: [], tags: [] };
+import { manageBookmarkData } from "./bookmark-data-management.js";
 const bookmarkActive = document.getElementsByClassName("active");
 const bookmark_button = document.getElementById("bookmark-button");
 bookmark_button.addEventListener("click", () => {
@@ -8,40 +6,7 @@ bookmark_button.addEventListener("click", () => {
     const tab = tabs[0];
 
     const newBookmark = { title: tab.title, url: tab.url };
-    const arr = [];
-    for (let i = 0; i < bookmarkActive.length; i++) {
-      arr.push(bookmarkActive[i].innerText);
-    }
-    // get data (bookmarkObject) and then add it to the array and then set it to the storage again
-    
-    get("bookmarkObject")
-      .then((existingData) => {
-        
-        if (!existingData) {
-          existingData = bookmarkObject; // if no data is present, initialize it with an empty object (bookmarkObject)
-        }
-    
-        if (!existingData.bookmarks) {
-          existingData.bookmarks = []; // if no bookmarks array is present, initialize it with an empty array
-        }
-    
-        if (!existingData.tags) {
-          existingData.tags = []; // some here but for tags
-        }
-    
-        const existingUrls = existingData.bookmarks.map(
-          (bookmark) => bookmark.url
-        );
-        if (!existingUrls.includes(newBookmark.url)) {
-          existingData.bookmarks.push(newBookmark);
-          existingData.tags.push(arr);
-          return set("bookmarkObject", existingData);
-        }
-
-        return existingData;
-      })
-      .then(() => {
-        get("bookmarkObject").then((data) => console.log(data));
-      });
+    // call managebookmarkdata function to save the data to the storage
+    manageBookmarkData(bookmarkActive, newBookmark);
   });
 });
---
+++ scripts/bookmark-data-management.js
@@ -0,0 +1,40 @@
+import { get, set, remove, removeAll } from "./data/storage.js";
+const bookmarkObject = { bookmarks: [], tags: [] };
+
+export function manageBookmarkData(bookmarkActive , newBookmark){
+    const arr = [];
+    for (let i = 0; i < bookmarkActive.length; i++) {
+      arr.push(bookmarkActive[i].innerText);
+    }
+    // get data (bookmarkObject) and then add it to the array and then set it to the storage again
+    
+    get("bookmarkObject")
+      .then((existingData) => {
+        
+        if (!existingData) {
+          existingData = bookmarkObject; // if no data is present, initialize it with an empty object (bookmarkObject)
+        }
+    
+        if (!existingData.bookmarks) {
+          existingData.bookmarks = []; // if no bookmarks array is present, initialize it with an empty array
+        }
+    
+        if (!existingData.tags) {
+          existingData.tags = []; // some here but for tags
+        }
+    
+        const existingUrls = existingData.bookmarks.map(
+          (bookmark) => bookmark.url
+        );
+        if (!existingUrls.includes(newBookmark.url)) {
+          existingData.bookmarks.push(newBookmark);
+          existingData.tags.push(arr);
+          return set("bookmarkObject", existingData);
+        }
+
+        return existingData;
+      })
+      .then(() => {
+        get("bookmarkObject").then((data) => console.log(data));
+      });
+}
