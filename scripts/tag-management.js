5290805e906735faf95fbe52d00345482bd7dc20
zombie22 <fatihkabbani223@gmail.com>
2023-08-09 17:57:00 +0200

    removed the icon and added an image

--- scripts/tag-management.js
+++ scripts/tag-management.js
@@ -5,7 +5,7 @@ tagDelete.addEventListener("click", () => {
   for (let i = 0; i < tags.length; i++) {
     tags[i].classList.toggle("t-delete");
     if (tags[i].classList.contains("t-delete")) {
-      tags[i].innerHTML += `<i class="fas fa-trash-alt tag-trash-can"></i>`;
+      tags[i].innerHTML += `<img src="../images/icons/trash.png" />`;
     }else{
         tags[i].innerHTML = tags[i].innerHTML.slice(0, -37);
     }
