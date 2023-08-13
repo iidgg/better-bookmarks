4c7e0e6d7f8a9df5f890d20c996381d8e3f41e29
zombie22 <fatihkabbani223@gmail.com>
2023-08-13 08:08:58 +0200

    optimized display-tags.js

--- scripts/display-tgas.js
+++ scripts/display-tgas.js
@@ -1,18 +1,48 @@
-import {get , set , remove , removeAll} from "./data/storage.js";
+import { get, set, remove, removeAll } from "./data/storage.js";
+
 const bookmarkContainer = document.getElementById("bookmark-container");
 const tagListContainer = document.getElementById("tag-list-container");
-const tags = await get("tags").then((data) => data);
-const values = await get("bookmarkObject").then((data) => data);
-for(let i = 0; i < values.bookmarks.length; i++){
-    bookmarkContainer.innerHTML += `<li><a href=${values.bookmarks[i].url}> ${values.bookmarks[i].title} <span>tags:  ${ values.bookmarks[i].tags.join(" ")}  </span></a></li>` ; 
-}
-for(let i= 0; i < tags.length; i++){
-    tagListContainer.innerHTML += `<li class="liContainer"><span>${tags[i]}</span></li>` ;
+
+async function fetchData() {
+  try {
+    const [tags, values] = await Promise.all([
+      get("tags"),
+      get("bookmarkObject")
+    ]);
+
+    displayBookmarks(values.bookmarks);
+    displayTags(tags);
+    setupTagClickListeners();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
-const liContainer = document.getElementsByClassName("liContainer");
-for(let i = 0; i < liContainer.length; i++){
-    liContainer[i].addEventListener("click" , () => {
-        liContainer[i].classList.toggle("tag-active");
-    })
+function displayBookmarks(bookmarks) {
+  bookmarkContainer.innerHTML = bookmarks
+    .map(bookmark =>`<li><a href="${bookmark.url}">${bookmark.title}<span>tags: ${bookmark.tags.join(" ")}</span></a></li>`).join("");
 }
+
+function displayTags(tags) {
+  tagListContainer.innerHTML = tags
+    .map(tag => `<li class="liContainer"><span>${tag}</span></li>`)
+    .join("");
+}
+
+function setupTagClickListeners() {
+  const liContainer = document.querySelectorAll(".liContainer");
+  liContainer.forEach(container => {
+    container.addEventListener("click", () => {
+      container.classList.toggle("tag-active");
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", fetchData);
+
+// const liContainer = document.getElementsByClassName("liContainer");
+// for(let i = 0; i < liContainer.length; i++){
+//     liContainer[i].addEventListener("click" , () => {
+//         liContainer[i].classList.toggle("tag-active");
+//     })
+// }
