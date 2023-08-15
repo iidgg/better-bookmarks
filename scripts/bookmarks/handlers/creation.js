f1cf99108bf900ef877ed0470b5ce538a8a0eae4
zombie22 <fatihkabbani223@gmail.com>
2023-08-15 16:43:07 +0200

    modified:   manifest.json 	modified:   scripts/bookmarks/handlers/creation.js 	modified:   scripts/prepare-frames.js

--- scripts/bookmarks/handlers/creation.js
+++ scripts/bookmarks/handlers/creation.js
@@ -1,18 +1,19 @@
 import { manageBookmarkData } from "./../../bookmark-data-management.js";
+import { prepareFrame } from "./../../prepare-frames.js";
 // import { removeAll } from "./data/storage.js";
 
 const bookmarkActive = document.getElementsByClassName("active");
 const bookmark_button = document.getElementById("bookmark-button");
 
 bookmark_button.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
     const tab = tabs[0];
     chrome.bookmarks.create({ title: tab.title, url: tab.url });
     const arr = [];
     for (let i = 0; i < bookmarkActive.length; i++) {
       arr.push(bookmarkActive[i].innerText);
     }
-    const newBookmark = { title: tab.title, url: tab.url, tags: arr , screenshot: captureVisibleTab() };
+    const newBookmark = { title: tab.title, url: tab.url, tags: arr , screenshot:  {data: await prepareFrame(tab.id)}};
     // call managebookmarkdata function to save the data to the storage
     // TODO: dumpy code here
     manageBookmarkData(bookmarkActive, newBookmark);
--- scripts/prepare-frames.js
+++ scripts/prepare-frames.js
@@ -1,8 +1,8 @@
 // TODO make it take a picture instade 
 export function prepareFrame() {
   return new Promise((resolve) => {
-    chrome.tabs.captureVisibleTab({ format: "png" }, (screenshotDataUrl) => {
-      console.log(screenshotDataUrl)
+    chrome.tabs.captureVisibleTab({ format: "png"  }, (screenshotDataUrl) => {
+      // console.log(screenshotDataUrl)
       resolve(screenshotDataUrl);
     });
   });
