784ab673a858e61d88aaa58614526010173bee53
zombie22 <fatihkabbani223@gmail.com>
2023-08-16 05:55:27 +0200

    commented folder-list (style script html)

--- scripts/bookmarks/list.js
+++ scripts/bookmarks/list.js
@@ -1,35 +1,35 @@
-import { goThroughBranch } from "../functions.js";
-import { bookmarkELement, folderElement } from "./element.js";
-//? List the bookmarks
+// import { goThroughBranch } from "../functions.js";
+// import { bookmarkELement, folderElement } from "./element.js";
+// //? List the bookmarks
 
-chrome.bookmarks.getTree().then((tree) => {
-  goThroughBranch(tree, async function (treeElement) {
-    // console.log("new operation"); i don't think we need this 999999 console.log messages >:(
-    try {
-      let elements;
-      const id = treeElement.id;
-      const title = treeElement.title;
-      const parentID = treeElement.parentId || "0";
-      const parentElement = document.getElementById(`treeID-${parentID}`);
+// chrome.bookmarks.getTree().then((tree) => {
+//   goThroughBranch(tree, async function (treeElement) {
+//     // console.log("new operation"); i don't think we need this 999999 console.log messages >:(
+//     try {
+//       let elements;
+//       const id = treeElement.id;
+//       const title = treeElement.title;
+//       const parentID = treeElement.parentId || "0";
+//       const parentElement = document.getElementById(`treeID-${parentID}`);
 
-      treeElement.children
-        ? (elements = folderElement(id, title))
-        : (elements = bookmarkELement(id, title));
-      elements.forEach((ge) => {
-        // ge = given element
-        parentElement.appendChild(ge);
-      });
-    } catch (err) {
-      console.error("Catch error", err);
-    }
-  });
+//       treeElement.children
+//         ? (elements = folderElement(id, title))
+//         : (elements = bookmarkELement(id, title));
+//       elements.forEach((ge) => {
+//         // ge = given element
+//         parentElement.appendChild(ge);
+//       });
+//     } catch (err) {
+//       console.error("Catch error", err);
+//     }
+//   });
 
-  const allFolders = document.querySelectorAll(".folder-list .folder-title");
-  allFolders.forEach((ft) => {
-    // ft = folder title (button)
-    ft.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector(`#${ft.for}`).classList.toggle("active-parent");
-    });
-  });
-});
+//   const allFolders = document.querySelectorAll(".folder-list .folder-title");
+//   allFolders.forEach((ft) => {
+//     // ft = folder title (button)
+//     ft.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       document.querySelector(`#${ft.for}`).classList.toggle("active-parent");
+//     });
+//   });
+// });
